a2d577a991fb92af8aeb3784353758aae85a0013
Vinay Kumar <vinaymavi@gmail.com>
2025-11-05 16:08:49 +0530

    feat: add new profile images and enhance HeroTile component with dynamic content

--- src/consts.ts
+++ src/consts.ts
@@ -5,3 +5,8 @@ export const SITE_TITLE = "VinayMavi";
 export const SITE_DESCRIPTION = "Welcome to Vinay Mavi Blogs";
 export const GITHUB_PROFILE = "https://github.com/vinaymavi";
 export const MY_NAME = "Vinay Mavi";
+
+export const INTRO2 =
+  "I truly belive that AI enabing software engineers to dlivery more so that every egineer can delivery more for example Now engineering and automate that task on every fast face thats not all it also enables prodcut to explore new client problems on fast pace. ";
+export const INTRO3 =
+  "My skill set serves as a one-stop solution for all your product challenges. I bring strong expertise across the full spectrum of software development — from designing intuitive, high-performing client-facing interfaces to building scalable and efficient backends that power intelligent, AI-driven systems. Whether it’s crafting seamless user experiences, architecting robust infrastructure, or integrating smart agents into complex workflows, I’m equipped to deliver end-to-end solutions that align technology with business goals.";
